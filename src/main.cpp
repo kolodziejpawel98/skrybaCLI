907a2e144389c050a2646f1355b42b4b5b761aaf
kolodziejpawel98 <kolodziejpawel98@gmail.com>
2024-07-06 18:27:14 +0200

    Coloring bye-text

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,23 @@
 #include <ncurses.h>
 #include <cstring>
+#include <iostream>
+
+enum textColor
+{
+    red_black = 1,
+};
+
+namespace cursor
+{
+    unsigned short row, col;
+};
+
+void printTextInColor(std::string text, unsigned short x, unsigned short y)
+{
+    mvprintw(x, y, text.c_str());
+    getyx(stdscr, cursor::row, cursor::col);
+    mvchgat(cursor::row, 0, text.length(), A_BOLD, textColor::red_black, NULL);
+}
 
 int main()
 {
@@ -9,17 +27,22 @@ int main()
     cbreak();
     keypad(stdscr, TRUE);
     echo();
+    start_color();
+    init_pair(textColor::red_black, COLOR_RED, COLOR_BLACK);
 
-    mvprintw(10, 0, "Type 'q' to quit\n");
+    mvprintw(0, 0, "Type 'q' to quit\n");
     char quit_string[10];
+
     while (true)
     {
         getstr(quit_string);
         if (!strcmp(quit_string, "quit"))
         {
-            printw("\nbye...");
+            getyx(stdscr, cursor::row, cursor::col);
+            printTextInColor("Made by PK", cursor::row + 1, 0);
+            printTextInColor("bye...", cursor::row + 1, 0);
             refresh();
-            napms(400);
+            napms(600);
             break;
         }
         else
