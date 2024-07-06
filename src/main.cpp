ac636e02fb933e069d866d30357c82f33a8e3f59
kolodziejpawel98 <kolodziejpawel98@gmail.com>
2024-07-06 20:25:04 +0200

    buttons nav with KEY_UP & KEY_DOWN

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #include <ncurses.h>
 #include <cstring>
 #include <iostream>
+#include <vector>
 
 enum textColor
 {
@@ -11,6 +12,8 @@ namespace cursor
 {
     unsigned short row, col;
 };
+
+std::vector<uint8_t> buttons = {1, 2, 3};
 
 void printTextInColor(std::string text, unsigned short x, unsigned short y)
 {
@@ -30,8 +33,15 @@ void drawFrame()
 
 void drawButtons()
 {
-    for (unsigned short i = 1; i <= 3; ++i)
+    for (unsigned short i = 1; i <= buttons.size(); ++i)
         mvprintw(i + 15, 4, "[ ]");
+}
+
+void drawStarCursor(uint8_t buttonFromTop)
+{
+    for (unsigned short i = 1; i <= buttons.size(); ++i)
+        mvprintw(i + 15, 4, "[ ]");
+    mvprintw(buttonFromTop + 15, 4, "[*]");
 }
 
 int main()
@@ -39,13 +49,16 @@ int main()
     int ch;
 
     initscr();
-    cbreak();
+    // cbreak();
+    raw();
     keypad(stdscr, TRUE);
-    echo();
+    noecho();
     start_color();
     init_pair(textColor::red_black, COLOR_RED, COLOR_BLACK);
     drawFrame();
     drawButtons();
+    drawStarCursor(1);
+    curs_set(0);
     mvprintw(1, 10, "Type 'q' to quit\n");
     char quit_string[10];
 
@@ -53,25 +66,57 @@ int main()
     {
         getyx(stdscr, cursor::row, cursor::col);
         move(cursor::row, cursor::col + 10);
-        getstr(quit_string);
-        if (!strcmp(quit_string, "quit"))
+        // getstr(quit_string);
+        ch = getch();
+
+        switch (ch)
         {
+        case 'q':
             getyx(stdscr, cursor::row, cursor::col);
             printTextInColor("Made by PK", cursor::row + 1, 10);
             printTextInColor("bye...", cursor::row + 1, 10);
             refresh();
             napms(500);
+            endwin();
+            return 0;
+        case KEY_UP:
+            drawStarCursor(1);
+            // getyx(stdscr, cursor::row, cursor::col);
+            // mvprintw(cursor::row, cursor::col + 1, "KEY_UP was pressed! Try again.\n");
+            break;
+        case KEY_DOWN:
+            drawStarCursor(2);
+            // getyx(stdscr, cursor::row, cursor::col);
+            // mvprintw(cursor::row, cursor::col + 1, "KEY_DOWN was pressed! Try again.\n");
+            break;
+        case '\n':
+            getyx(stdscr, cursor::row, cursor::col);
+            mvprintw(cursor::row, cursor::col + 10, "ENTER was pressed! Try again.\n");
+            break;
+        default:
+            getyx(stdscr, cursor::row, cursor::col);
+            mvprintw(cursor::row, cursor::col + 1, "%s was pressed! Try again.\n", quit_string);
             break;
         }
-        else
-        {
-            getyx(stdscr, cursor::row, cursor::col);
-            mvprintw(cursor::row, cursor::col + 10, "%s was pressed! Try again.\n", quit_string);
-        }
-        refresh();
     }
-
     endwin();
 
-    return 0;
+    //     if (ch == KEY_UP)
+    //     {
+    // getyx(stdscr, cursor::row, cursor::col);
+    // printTextInColor("Made by PK", cursor::row + 1, 10);
+    // printTextInColor("bye...", cursor::row + 1, 10);
+    // refresh();
+    // napms(500);
+    // break;
+    //     }
+    //     else
+    //     {
+    //         getyx(stdscr, cursor::row, cursor::col);
+    //         mvprintw(cursor::row, cursor::col + 10, "%s was pressed! Try again.\n", quit_string);
+    //     }
+    //     refresh();
+    // }
+
+    // endwin();
 }
