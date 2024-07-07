ffd2d76e844af7b5617f846fbfb55b37199c6a00
kolodziejpawel98 <kolodziejpawel98@gmail.com>
2024-07-07 11:53:15 +0200

    Button class  with navigation + debugPrint method

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,7 @@
 enum textColor
 {
     red_black = 1,
+    white_black
 };
 
 namespace cursor
@@ -13,7 +14,70 @@ namespace cursor
     unsigned short row, col;
 };
 
-std::vector<uint8_t> buttons = {1, 2, 3};
+void debugPrint(std::string text)
+{
+    int oldRow, oldCol;
+    getyx(stdscr, oldRow, oldCol);
+    mvprintw(40, 15, text.c_str());
+
+    move(oldRow, oldCol);
+}
+
+class Button
+{
+public:
+    Button(std::string labelText, uint16_t row, uint16_t column) : labelText(labelText),
+                                                                   row(row),
+                                                                   column(column){};
+    std::string labelText;
+    uint16_t row, column;
+
+    void draw(bool isCursorOnMe = false)
+    {
+        std::string buttonAndText;
+        if (!isCursorOnMe)
+        {
+            attroff(A_BOLD | COLOR_PAIR(textColor::red_black));
+            buttonAndText = "[ ]  " + labelText;
+            mvprintw(row, column, buttonAndText.c_str());
+        }
+
+        else
+        {
+            attron(A_BOLD | COLOR_PAIR(textColor::red_black));
+            buttonAndText = "[*]  " + labelText;
+            mvprintw(row, column, buttonAndText.c_str());
+        }
+    }
+};
+
+std::vector<Button> buttons = {
+    Button("option 1", 15, 4), Button("option 2", 16, 4), Button("option 3", 17, 4), Button("option 4", 18, 4)};
+
+auto starCursor = buttons.begin();
+void lowerButton()
+{
+    if (starCursor != buttons.end() - 1)
+    {
+        ++starCursor;
+    }
+    else
+    {
+        starCursor = buttons.begin();
+    }
+}
+
+void upperButton()
+{
+    if (starCursor != buttons.begin())
+    {
+        --starCursor;
+    }
+    else
+    {
+        starCursor = buttons.end() - 1;
+    }
+}
 
 void printTextInColor(std::string text, unsigned short x, unsigned short y)
 {
@@ -33,14 +97,17 @@ void drawFrame()
 
 void drawButtons()
 {
-    for (unsigned short i = 1; i <= buttons.size(); ++i)
-        mvprintw(i + 15, 4, "[ ]");
+    for (auto &button : buttons)
+        button.draw();
+    starCursor->draw(true);
 }
 
 void drawStarCursor(uint8_t buttonFromTop)
 {
     for (unsigned short i = 1; i <= buttons.size(); ++i)
         mvprintw(i + 15, 4, "[ ]");
+
+    attron(A_BOLD | COLOR_PAIR(textColor::red_black));
     mvprintw(buttonFromTop + 15, 4, "[*]");
 }
 
@@ -55,9 +122,11 @@ int main()
     noecho();
     start_color();
     init_pair(textColor::red_black, COLOR_RED, COLOR_BLACK);
+    init_pair(textColor::white_black, COLOR_WHITE, COLOR_BLACK);
     drawFrame();
     drawButtons();
-    drawStarCursor(1);
+    attroff(A_BOLD | COLOR_PAIR(textColor::red_black));
+    // drawStarCursor(1);
     curs_set(0);
     mvprintw(1, 10, "Type 'q' to quit\n");
     char quit_string[10];
@@ -65,13 +134,14 @@ int main()
     while (true)
     {
         getyx(stdscr, cursor::row, cursor::col);
-        move(cursor::row, cursor::col + 10);
+        move(cursor::row, 4);
         // getstr(quit_string);
         ch = getch();
 
         switch (ch)
         {
         case 'q':
+            move(10, 4);
             getyx(stdscr, cursor::row, cursor::col);
             printTextInColor("Made by PK", cursor::row + 1, 10);
             printTextInColor("bye...", cursor::row + 1, 10);
@@ -80,43 +150,23 @@ int main()
             endwin();
             return 0;
         case KEY_UP:
-            drawStarCursor(1);
-            // getyx(stdscr, cursor::row, cursor::col);
-            // mvprintw(cursor::row, cursor::col + 1, "KEY_UP was pressed! Try again.\n");
+            upperButton();
+            drawButtons();
             break;
         case KEY_DOWN:
-            drawStarCursor(2);
-            // getyx(stdscr, cursor::row, cursor::col);
-            // mvprintw(cursor::row, cursor::col + 1, "KEY_DOWN was pressed! Try again.\n");
+            lowerButton();
+            drawButtons();
             break;
         case '\n':
             getyx(stdscr, cursor::row, cursor::col);
             mvprintw(cursor::row, cursor::col + 10, "ENTER was pressed! Try again.\n");
             break;
         default:
+            move(10, 4);
             getyx(stdscr, cursor::row, cursor::col);
-            mvprintw(cursor::row, cursor::col + 1, "%s was pressed! Try again.\n", quit_string);
+            mvprintw(cursor::row, cursor::col, "%c was pressed! Try again.\n", ch);
             break;
         }
     }
     endwin();
-
-    //     if (ch == KEY_UP)
-    //     {
-    // getyx(stdscr, cursor::row, cursor::col);
-    // printTextInColor("Made by PK", cursor::row + 1, 10);
-    // printTextInColor("bye...", cursor::row + 1, 10);
-    // refresh();
-    // napms(500);
-    // break;
-    //     }
-    //     else
-    //     {
-    //         getyx(stdscr, cursor::row, cursor::col);
-    //         mvprintw(cursor::row, cursor::col + 10, "%s was pressed! Try again.\n", quit_string);
-    //     }
-    //     refresh();
-    // }
-
-    // endwin();
 }
