3d725eae48eeba794d15bb2a9ab7d911d9dcc4e0
kolodziejpawel98 <kolodziejpawel98@gmail.com>
2024-07-13 21:52:52 +0200

    Month name + arrow bug fix

--- src/screens/new_month_intro.cpp
+++ src/screens/new_month_intro.cpp
@@ -6,8 +6,7 @@ void NewMonthIntro::setup()
     refresh();
     drawFrame();
     drawBanner();
-    debugPrint("NEW MONTH INTRO");
-    buttons.emplace_back(Button(inputWord, 38, 4, NEW_MONTH_CREATOR));
+    buttons.emplace_back(Button("Dalej", 38, 4, NEW_MONTH_CREATOR));
     buttons.emplace_back(Button("Cofnij", 39, 4, MAIN_MENU));
     if (!buttons.empty())
     {
@@ -15,16 +14,15 @@ void NewMonthIntro::setup()
     }
     drawButtons(buttons, starCursor);
     printTextInColor(inputWord, 30, LEFT_MARGIN + 40);
-
-    inputWord = "[]";
 }
 
 void NewMonthIntro::loop()
 {
-    printTextInColor(monthName, 10, LEFT_MARGIN + 40);
-    refresh();
+
     while (currentScreen == NEW_MONTH_INTRO)
     {
+        printTextInColor("Month name: " + monthName, 10, LEFT_MARGIN + 40);
+        refresh();
         inputChar = getch();
         if (std::isalpha(inputChar) ||
             inputChar == '\n' ||
@@ -43,24 +41,30 @@ void NewMonthIntro::loop()
                 drawButtons(buttons, starCursor);
                 break;
             case '\n': // ENTER
-                monthName = inputWord;
+                monthName = inputWord.substr(1, inputWord.size() - 2);
                 inputWord = "[]";
                 currentScreen = starCursor->getPointingToScreen();
                 break;
             case KEY_BACKSPACE:
                 if (inputWord.length() > 2)
                 {
-                    debugPrint("USUWANA LITERA: " + std::string(1, inputWord.at(inputWord.length() - 2)), 10, 10);
                     inputWord.erase(inputWord.length() - 2, 1);
-                    printTextInColor(inputWord, 38, LEFT_MARGIN + 5);
+                    clear();
+                    refresh();
+                    drawFrame();
+                    drawBanner();
+                    drawButtons(buttons, starCursor);
+                    printTextInColor(inputWord, 30, LEFT_MARGIN + 40);
                 }
                 break;
             default:
-                if (starCursor->getRow() == 38)
-                {
-                    inputWord.insert(inputWord.size() - 1, 1, inputChar);
-                    printTextInColor(inputWord, 38, LEFT_MARGIN + 5);
-                }
+                inputWord.insert(inputWord.size() - 1, 1, inputChar);
+                clear();
+                refresh();
+                drawFrame();
+                drawBanner();
+                drawButtons(buttons, starCursor);
+                printTextInColor(inputWord, 30, LEFT_MARGIN + 40);
                 break;
             }
         }
